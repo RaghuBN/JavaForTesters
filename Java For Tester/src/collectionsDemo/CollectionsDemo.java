0ed9b05242671bbd9a48dbd7f3ca61553332f023
RaghuBN <raghu.astepahead@gmail.com>
2019-11-05 07:52:50 +0530

    Collection Demo

---
+++ Java For Tester/src/collectionsDemo/CollectionsDemo.java
@@ -0,0 +1,46 @@
+package collectionsDemo;
+
+import java.util.*;
+public class CollectionsDemo {
+	public static void main(String[] args) {
+        // ArrayList
+        List < String > list = new ArrayList < > ();
+        list.add("element 1");
+        list.add("element 2");
+        list.add("element 3");
+        list.add("element 4");
+        System.out.println(" ArrayList Elements");
+        System.out.print("\t" + list);
+
+        // LinkedList
+        List < String > linkedList = new LinkedList < > ();
+        linkedList.add("element 1");
+        linkedList.add("element 2");
+        linkedList.add("element 3");
+        linkedList.add("element 4");
+        System.out.println();
+        System.out.println(" LinkedList Elements");
+        System.out.print("\t" + linkedList);
+
+        // HashSet
+        Set < String > set = new HashSet < > ();
+        set.add("element 1");
+        set.add("element 2");
+        set.add("element 3");
+        set.add("element 4");
+        System.out.println();
+        System.out.println(" Set Elements");
+        System.out.print("\t" + set);
+
+        // HashMap
+        Map < String, String > map = new HashMap < > ();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key3", "value3");
+        map.put("key4", "value4");
+
+        System.out.println();
+        System.out.println(" Map Elements");
+        System.out.print("\t" + map);
+    }
+}
